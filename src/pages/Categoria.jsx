ecc315cd263ef2b99e67bd7dd49e4fede152b72f
BryantGG77 <brayespro@gmail.com>
2024-12-09 13:45:06 -0500

    Proyecto final

--- src/pages/Categoria.jsx
+++ src/pages/Categoria.jsx
@@ -3,15 +3,19 @@ import "../assets/css/blog.css"
 import { buscar } from "../api/api";
 import ListCategories from "../components/ListCategories";
 import ListPosts from "../components/ListPosts";
-import { useParams, Routes, Route } from "react-router-dom";
+import SubCategoria from "./SubCategoria";
+import { useParams, Routes, Route, Link, useResolvedPath } from "react-router-dom";
 const Categoria = () => {
-    const [subCategorias, setsubCategorias] = useState([]);
+    const [subcategorias, setSubcategorias] = useState([]);
     const { id } = useParams();
+
+    const url = useResolvedPath("").pathname;
+
 
 
     useEffect(() => {
         buscar(`/categorias?id=${id}`, (response) => {
-            setsubCategorias(response[0].subcategorias);
+            setSubcategorias(response[0].subcategorias);
         });
     }, [id]);
 
@@ -21,8 +25,20 @@ const Categoria = () => {
                 <h2 className="title-page">Pet noticias</h2>
             </div>
             <ListCategories />
+            <ul className='category-list container flex'>
+                {
+                    subcategorias.map(subcategoria => (
+                        <li className={`category-list__category category-list__category--${id}`} key={subcategoria}>
+                            <Link to={`${url}/${subcategoria}`}>
+                                {subcategoria}
+                            </Link>
+                        </li>
+                    ))
+                }
+            </ul>
             <Routes>
                 <Route path="/" element={<ListPosts url={`/posts?categoria=${id}`} />} />
+                <Route path="/:subcategoria" element={<SubCategoria />} />
             </Routes>
 
         </>
